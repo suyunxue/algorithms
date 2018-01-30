2dab81853ae4f29ad4a2db7fed3d5c59cc9c43cc
sufubo <sufubo@baidu.com>
2018-01-30 10:43:03 +0800

    插入排序

---
+++ algorithmIntroduction/src/insertionSort.js
@@ -0,0 +1,20 @@
+function insertionSrot(arr) {
+    var key, i;
+    for (var j = 1; j < arr.length; j++) {
+        key = arr[j];
+        i = j - 1;
+        while (i > 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            i = i - 1;
+        }
+        arr[i + 1] = key;
+    }
+
+    console.log(arr);
+
+    return arr;
+}
+
+var arr = [1, 4, 32, 6, 43];
+insertionSrot(arr);
+
