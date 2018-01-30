e131fc40f406273b1b193f8fcf85de51d8138a88
sufubo <731644018@qq.com>
2018-01-30 23:56:15 +0800

    插入排序升序和降序

--- algorithmIntroduction/src/insertionSort.js
+++ algorithmIntroduction/src/insertionSort.js
@@ -1,4 +1,4 @@
-function insertionSrot(arr) {
+function insertionSort(arr) {
     var key, i;
     for (var j = 1; j < arr.length; j++) {
         key = arr[j];
@@ -15,5 +15,23 @@ function insertionSrot(arr) {
     return arr;
 }
 
+function insertSortDown(arr) {
+    var key, i;
+    for (var j = 1; j < arr.length; j++) {
+        key = arr[j];
+        i = j - 1;
+        while (i >= 0 && arr[i] < key) {
+            arr[i + 1] = arr[i];
+            i = i - 1;
+        }
+        arr[i + 1] = key;
+    }
+
+    console.log(arr);
+
+    return arr;
+}
+
 var arr = [23, 4, 32, 6, 43, 56, 13, 2];
-insertionSrot(arr);
+insertionSort(arr);
+insertSortDown(arr);
