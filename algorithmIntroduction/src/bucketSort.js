3fa31b5c1045f0e6faf9e20e9266a3ef77c0c2c5
sufubo <731644018@qq.com>
2018-04-21 00:07:30 +0800

    桶排序

--- algorithmIntroduction/src/bucketSort.js
+++ algorithmIntroduction/src/bucketSort.js
@@ -2,149 +2,67 @@
  * 桶排序参考文章：http://www.cnblogs.com/ywang1724/p/3946339.html
  */
 
-
 // 桶排序
-function bucketSort(arr, num) {
-    if (arr.length <= 1) {
-        return array;
+// 桶排序是有多个数字在里面，把他们都放在一个桶里面
+// 和基数排序有点像我们就把这写数据都按照a的每个数据都必须小于1，然后把数字都放入每个桶里面
+// 如何放入呢，这里就有我这里就是首位数字，然后首位数据就是他的桶，然后再用数组进行插入排序
+// 1.初始化为小于1的数字
+// 2.开始遍历进入桶
+// 3.碰到不是空桶就开始插入排序
+function bucketSort(arr) {
+    var maxNum = 0;
+    var curNum = 0;
+    var B = new Array(arr.length);
+    var len = arr.length;
+
+    for (var i = 0; i < len; i++) {
+        maxNum = maxNum < arr[i].toString().length ? arr[i].toString().length : maxNum;
     }
 
+    // 应该除得是最大的
+    for (var i = 0; i < len; i++) {
+        arr[i] = arr[i] / (Math.pow(10, maxNum));
+    }
 
+    console.log(arr);
+
+    for (var i = 0; i < len; i++) {
+        var index = Math.floor(arr[i] * 10);
+        console.log(index)
+        if (B[index]) {
+
+            // 插入排序
+            var k = B[index].length - 1;
+            while (k >= 0 && B[index][k] > arr[i]) {
+                B[index][k + 1] = B[index][k];
+                k--;
+            }
+            B[index][k + 1] = arr[i];
+        }
+        else {
+            B[index] = [];
+            B[index].push(arr[i]);
+        }
+    }
+
+    var res = [];
+    // 操作完毕之后把桶里面不为空值的拼接起来
+    for (var i = 0; i < B.length; i++) {
+        if (B[i]) {
+            res = res.concat(B[i]);
+        }
+    }
+
+    // 恢复正常
+    for (var i = 0; i < len; i++) {
+        res[i] = res[i] * Math.pow(10, maxNum);
+    }
+
+    return res;
 }
 
 var arr = [323, 43, 32, 5, 32, 43, 45];
-bucketSort(arr);
-console.log(arr);
+var res = bucketSort(arr);
+console.log(res);
 
-function linkedList() {
-    var Node = function (ele) {
-        this.ele = ele;
-        this.next = null;
-    };
-
-    let length = 0;
-
-    let head = null;
-
-    this.append = function(element){
-        //初始化添加的Node实例
-        let node = new Node(element),
-            current;
-        if (head === null){
-            //第一个Node实例进入链表，之后在这个LinkedList实例中head就不再是null了
-            head = node;
-        } else {
-            current = head;
-            //循环链表知道找到最后一项，循环结束current指向链表最后一项元素
-            while(current.next){
-                current = current.next;
-            }
-            //找到最后一项元素后，将他的next属性指向新元素node,j建立链接
-            current.next = node;
-        }
-        //更新链表长度
-        length++;
-    };
-
-    this.insert = function(position, element){
-        //检查是否越界，超过链表长度或是小于0肯定不符合逻辑的
-        if (position >= 0 && position <= length){
-            let node = new Node(element),
-                current = head,
-                previous,
-                index = 0;
-            if (position === 0){
-                //在第一个位置添加
-                node.next = current;
-                head = node;
-            } else {
-                //循环链表，找到正确位置，循环完毕，previous，current分别是被添加元素的前一个和后一个元素
-                while (index++ < position){
-                    previous = current;
-                    current = current.next;
-                }
-                node.next = current;
-                previous.next = node;
-            }
-            //更新链表长度
-            length++;
-            return true;
-        } else {
-            return false;
-        }
-    };
-
-    this.removeAt = function(position){
-        //检查是否越界，超过链表长度或是小于0肯定不符合逻辑的
-        if (position > -1 && position < length){
-            let current = head,
-                previous,
-                index = 0;
-            //移除第一个元素
-            if (position === 0){
-                //移除第一项，相当于head=null;
-                head = current.next;
-            } else {
-                //循环链表，找到正确位置，循环完毕，previous，current分别是被添加元素的前一个和后一个元素
-                while (index++ < position){
-                    previous = current;
-                    current = current.next;
-                }
-                //链接previous和current的下一个元素，也就是把current移除了
-                previous.next = current.next;
-            }
-            length--;
-            return current.element;
-        } else {
-            return null;
-        }
-    };
-
-    this.indexOf = function(element){
-        let current = head,
-            index = 0;
-        //循环链表找到元素位置
-        while (current) {
-            if (element === current.element) {
-                return index;
-            }
-            index++;
-            current = current.next;
-        }
-        return -1;
-    };
-
-    this.remove = function(element){
-        //调用已经声明过的indexOf和removeAt方法
-        let index = this.indexOf(element);
-        return this.removeAt(index);
-    };
-
-    this.isEmpty = function() {
-        return length === 0;
-    };
-
-    this.size = function() {
-        return length;
-    };
-
-    this.getHead = function(){
-        return head;
-    };
-
-    this.toString = function(){
-        let current = head,
-            string = '';
-        while (current) {
-            string += current.element + (current.next ? ', ' : '');
-            current = current.next;
-        }
-        return string;
-    };
-
-    this.print = function(){
-        console.log(this.toString());
-    };
-
-}
 
