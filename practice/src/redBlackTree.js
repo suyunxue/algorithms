49d05f415864beff7cdc6a5b91003e88086dd351
sufubo <731644018@qq.com>
2018-05-11 00:10:12 +0800

    红黑树

---
+++ practice/src/redBlackTree.js
@@ -0,0 +1,45 @@
+// https://blog.csdn.net/rth362147773/article/details/78014688
+// 红黑树前端js代码
+
+let ReadBlackTree = (function () {
+    let colors = {
+        RED: 0,
+        BLACK: 1
+    };
+
+    class Node {
+        constructor(key, color) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+            this.color = color;
+
+            this.flipColor = function () {
+                if (this.color === Colors.RED) {
+                    this.color = Colors.BLACK;
+                }
+                else {
+                    this.color = Colors.RED;
+                }
+            }
+        }
+    }
+
+    class RedBlackTree {
+        constructor() {
+            this.root = null;
+        }
+
+        getRoot() {
+            return this.root;
+        }
+
+        isRed(node) {
+            if (!node) {
+                return false;
+            }
+
+            return node.color === Colors.RED;
+        }
+    }
+})
