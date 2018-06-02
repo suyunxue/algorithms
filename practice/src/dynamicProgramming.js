955bd04e3dce514319e28d234058283eb304a612
sufubo <731644018@qq.com>
2018-06-02 18:15:07 +0800

    备忘录法动态规划

--- practice/src/dynamicProgramming.js
+++ practice/src/dynamicProgramming.js
@@ -1,21 +1,45 @@
-function matrixChainOrder(p) {
-    var n = p.length - 1;
-    for (var i = 0; i < n; i++) {
-        m[i, i] = 0;
+function fibcci(n) {
+    if (n <= 0) {
+        return n;
     }
-    for (var l = 2; l < n; l++) {
-        for (var i = 1; i < n - l + 1; i++) {
-            j = i + l - 1;
-            m[i, j] = Infinity;
-            for (var k = i; K < j - 1; k++) {
-                q = m[i, k] + m[k + 1, j] +
-                if (q < m[i, j]) {
-                    m[i, j] = q;
-                    s[i, j] = k;
-                }
-            }
-        }
+    var memo = new Array(n + 1);
+    for (var i = 0; i <= n; i++) {
+        memo[i] = -1;
     }
 
-    return m
+    return fib(n, memo);
 }
+
+function fib(n, memo) {
+    if (memo[n] != -1) {
+        return memo[n];
+    }
+
+    // 如果已经求出了fib(n)的值直接返回，否则将求出的值保存在Memo备忘录中
+    if (n < 2) {
+        memo[n] = 1;
+    }
+    else {
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    }
+
+    return memo[n];
+}
+
+
+// 递归方法
+// function fib(n) {
+//     if (n <= 0) {
+//         return 0;
+//     }
+//     else if (n === 1) {
+//         return 1;
+//     }
+//     else {
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
+
+
+console.log(fibcci(6));
+
