43bcedbab8c256557d3b476b1dc6d515fb4c8d1c
sufubo <sufubo@baidu.com>
2018-07-21 23:11:28 +0800

    最长回文字串马拉车算法

--- algorithmIntroduction/src/longestPalindrome.js
+++ algorithmIntroduction/src/longestPalindrome.js
@@ -1,7 +1,47 @@
 /*
 最长回文子串问题
+https: //www.cnblogs.com/grandyang/p/4464476.html
+https: //www.cnblogs.com/grandyang/p/4464476.html
 */
 
+// 马拉车算法
+function longestPalindromeThree(str) {
+    var t = '$#';
+    for (var i = 0; i < str.length; i++) {
+        t += str[i];
+        t += '#';
+    }
+
+    var p = []; // 表示以ti字符为中心的回文字串的半径
+    for (var i = 0; i < t.length * 2; i++) {
+        p[i] = 0;
+    }
+
+    // process t
+    var mx = 0; // 回文字串能延伸到最右端的位置
+    var id = 0; // id为最大回文字串中心的位置
+    var resLen = 0;
+    var resCenter = 0;
+    for (var i = 1; i < t.length; i++) {
+        p[i] = mx > i ? Math.min(p[2 * id - 1], mx - i) : 1;
+        // debugger;
+        // return ;
+        while (t[i + p[i]] === t[i - p[i]]) {
+            p[i]++;
+        }
+        if (mx < i + p[i]) {
+            mx = i + p[i];
+            id = i;
+        }
+        if (resLen < p[i]) {
+            resLen = p[i];
+            resCenter = i;
+        }
+    }
+    return str.substr((resCenter - resLen) / 2, resLen - 1);
+}
+
+// 传统方法解决
 function longestPalindromeOne(str) {
     var startIndex = 0;
     var left = 0;
@@ -47,7 +87,8 @@ function longestPalindromeOne(str) {
     return str.substr(startIndex, len);
 }
 
-function longestPalindrome(str) {
+// 动态规划解决
+function longestPalindromeTwo(str) {
     var dp = new Array(str.length);
     for (var i = 0; i < str.length; i++) {
         dp[i] = new Array(str.length);
@@ -83,8 +124,8 @@ function longestPalindrome(str) {
 }
 
 
-var str = 'eedegfdjdf';
+var str = 'eded';
 
-var res = longestPalindrome(str);
+var res = longestPalindromeThree(str);
 
 console.log(res);
