ca748f48fc0a4e76dc9651834cbc3c966b41bef5
sufubo <731644018@qq.com>
2018-11-09 10:50:52 +0800

    思路

--- algorithmIntroduction/src/bfs.js
+++ algorithmIntroduction/src/bfs.js
@@ -1,1 +1,10 @@
 // https://segmentfault.com/a/1190000011983269
+// 算法实现
+
+function bfs(G, s) {
+    for (let i = 0; i < G.length; i++) {
+        u.color = white;
+        u.d = Infinity;
+        u.bfs
+    }
+}
