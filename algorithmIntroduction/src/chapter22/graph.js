267dc9dc8fd0e3dca7ed6dd7075458c2f2a7f555
sufubo <731644018@qq.com>
2018-12-14 14:23:19 +0800

    dijkstra 算法

--- algorithmIntroduction/src/chapter22/graph.js
+++ algorithmIntroduction/src/chapter22/graph.js
@@ -164,6 +164,72 @@ function Graph() {
         console.log('explored ' + u);
     }
 
+    this.dijkstra = function (src) {
+        var dist = [];
+        var visited = [];
+        var length = this.graph.length;
+
+        for (var i = 0; i < length; i++) {
+            dist[i] = Infinity;
+            visited[src] = 0;
+        }
+        dist[src] = 0;
+
+        for (var i = 0; i < length - 1; i++) {
+            var u = minDistance(dist, visited);
+            visited[u] = true;
+
+            for (var v = 0; v < length; v++) {
+                if (!visited[v] &&
+                    this.graph[u][v] != 0 && dist[u] != Infinity 
+                    && dist[u] + this.graph[u][v] < dist[v]) {
+                        dist[v] = dist[u] + this.graph[u][v];
+                    }
+            }
+        }
+        return dist;
+    }
+
+    var minDistance = function (dist, visited) {
+        var min = Infinity;
+        var minIndex = -1;
+
+        for (var v = 0; v < dist.length; v++) {
+            if (visited[v] == false && dist[v] <= min) {
+                min = dist[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+
+    this.floydWarshall = function () {
+        var dist = [];
+        var length = this.graph.length;
+        var i;
+        var j;
+        var k;
+
+        for (i = 0; i < length; i++) {
+            dist[i] = [];
+            for (j = 0; j < length; j++) {
+                dist[i][j] = this.graph[i][j];
+            }
+        }
+
+        for (k = 0; k < length; k++) {
+            for (i = 0; i < length; i++) {
+                for (j = 0; j < length; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
 }
 
 function printNode(value) {
