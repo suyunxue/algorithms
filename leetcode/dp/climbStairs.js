e037449fd8de0076f4f3471b20e0b1ae353e5319
suyunxue <731644018@qq.com>
2019-11-09 12:21:04 +0800

    add climbStairs dp

--- leetcode/dp/climbStairs.js
+++ leetcode/dp/climbStairs.js
@@ -1,3 +1,22 @@
+// 动态规划版本
+var climbStairs = function (n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    var dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (var i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
+ 
+ 
+ 
  /**
   * @param {number} n
   * @return {number}
