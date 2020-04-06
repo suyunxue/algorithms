e6f65bab7b684361dbad20ce9ac778368ded1623
suyunxue <731644018@qq.com>
2020-04-06 23:04:39 +0800

    connect

--- leetcode/tree/connect.js
+++ leetcode/tree/connect.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<script>
 /**
  * // Definition for a Node.
  * function Node(val, left, right, next) {
@@ -54,6 +44,3 @@ var connect = function(root) {
 
     return root;
 };
-</script>
-</body>
-</html>
