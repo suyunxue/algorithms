5e2f5a2495643a375cbe7854afcb1ee1f859a832
suyunxue <731644018@qq.com>
2020-10-07 11:43:40 +0800

    add parenthes

--- leetcode/backtrack/generateParenthesis.js
+++ leetcode/backtrack/generateParenthesis.js
@@ -40,3 +40,29 @@ function valid(current) {
 
     return balance === 0;
 }
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    var ans = [];
+    backtrack(ans, [], 0, 0, n);
+    return ans;
+};
+
+function backtrack(ans, cur, open, close, max) {
+    if (cur.length === max * 2) {
+        ans.push(cur.join(''));
+    }
+    if (open < max) {
+        cur.push('(');
+        backtrack(ans, cur, open + 1, close, max);
+        cur.pop();
+    }
+    if (close < open) {
+        cur.push(')');
+        backtrack(ans, cur, open, close + 1, max);
+        cur.pop();
+    }
+}
