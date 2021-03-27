ba4020f0119017407defb97f39ff52d60db76173
suyunxue <731644018@qq.com>
2021-03-27 11:53:44 +0800

    add deleteNode.js

--- leetcode/LList/deleteNode.js
+++ leetcode/LList/deleteNode.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<script>
 function deleteNode(pListHead, pToBeDeleted) {
     if (!pListHead || !pToBeDeleted) {
         return;
@@ -30,6 +21,3 @@ function deleteNode(pListHead, pToBeDeleted) {
         pToBeDeleted = null;
     }
 }
-</script>
-</body>
-</html>
