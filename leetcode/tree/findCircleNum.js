51a4f5a7b8b7d8629c323cd31f79de6f65b07335
suyunxue <731644018@qq.com>
2021-08-07 16:48:40 +0800

    findCircleNum

--- leetcode/tree/findCircleNum.js
+++ leetcode/tree/findCircleNum.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<script>
 /**
  * @param {number[][]} isConnected
  * @return {number}
@@ -33,6 +24,3 @@
     }
     return counts;
 };
-</script>
-</body>
-</html>
